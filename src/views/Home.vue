e9bee7a38903fe4c335a5f3e245748c6b62e5b81
Aleksei Parm <alekseyparm@gmail.com>
2021-11-21 11:38:21 +0200

    Post component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
+    <Post authorPhoto="https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/45.png" date="14.02.2021 05:37" id="33" likes="1000" image="https://pics.me.me/when-you-actually-the-read-the-documentation-sometimes-my-genius-is-its-48199681.png" text="Test"></Post>
   </div>
 </template>
 
 <script>
 
+import Post from "../components/Post";
 export default {
   name: 'Home',
-  components: {}
+  components: {Post}
 }
 </script>
