138986b5a2d14032dade933f478bebb92e3b7e16
Aleksei Parm <alekseyparm@gmail.com>
2021-11-21 10:15:05 +0200

    About and Contacts views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et felis a sapien hendrerit maximus. Nulla vel risus quis nulla eleifend euismod quis ac eros. Donec vitae elementum ex. Proin eleifend, felis porta pharetra venenatis, turpis est varius magna, id vehicula erat orci sit amet est. Etiam euismod orci nisi, vitae vehicula mi laoreet a. Proin luctus quam sem, a laoreet tellus convallis vel. Proin interdum dictum turpis sed ultricies. Morbi lacinia mi ante, placerat ultricies mi tempor et. Morbi pretium at nibh vitae tincidunt. Sed feugiat vitae dolor vel ultricies. Suspendisse quis laoreet ligula. In blandit tellus nunc, eu iaculis nisl luctus sed. </p>
+    <p>Donec tincidunt elementum orci in imperdiet. Nunc vitae tincidunt libero. Maecenas quis sagittis diam. Cras in pulvinar dolor. Vivamus pharetra eros justo, nec iaculis metus blandit a. Sed interdum consequat dolor, in convallis odio faucibus et. Quisque vulputate luctus velit, nec feugiat nisi luctus ut. Duis vulputate mattis lacinia. Quisque sit amet neque pharetra, dapibus nisl ac, hendrerit ligula. Etiam nulla turpis, aliquam a dictum eget, dictum vel nisl. Nullam sed odio vehicula, laoreet orci ac, porta nisi. Nulla dapibus quam vel leo porttitor rutrum. </p>
+    <p>n ut sagittis velit, at tristique nibh. Cras eu nibh ligula. Maecenas sit amet nunc eget turpis efficitur molestie. Duis enim diam, tempus eget eros et, ornare gravida felis. Aliquam diam sem, laoreet suscipit elit vitae, finibus aliquam nisl. Proin eget placerat eros. Cras vel ex sit amet leo facilisis dictum nec non erat. Cras vitae eros sapien. </p>
   </div>
 </template>
+
+<style>
+.about {
+  width: 60%;
+  margin: auto;
+}
+
+.about p {
+  text-align: start;
+  font-size: 20px;
+}
+</style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="contacts">
-    <h1>Contacts page</h1>
+    <h1>Contacts</h1>
+    <p><b>Email:</b> info@example.com</p>
+    <p><b>Tel:</b> +0 123 4567</p>
+    <p><b>Address:</b> Ülikooli 1818, 50090, Tartu, Estonia</p>
   </div>
 </template>
+
+<style>
+.contacts {
+  margin-top: 90px;
+}
+</style>
