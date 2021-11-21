b3017e2158abac55d22fa593ce12ec4a4bbe7338
Aleksei Parm <alekseyparm@gmail.com>
2021-11-21 07:35:15 +0200

    Header and Footer components, Contacts view

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="footer">
+    <router-link :to="{ name: 'Contacts' }">Contact us</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style>
+#footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  background-color: aquamarine;
+
+  padding-top: 20px;
+}
+
+#footer a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Contacts' }">Contacts</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
